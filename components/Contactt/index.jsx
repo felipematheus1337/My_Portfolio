5abe1061a6b77ba265f024d0da723d4cee3557c6
felipematheus1337 <lipehbr@gmail.com>
2022-04-28 15:02:20 -0300

    finally

---
+++ components/Contactt/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Content } from './style'
+import { FaLinkedin, FaFacebook, FaInstagram, FaEnvelope, FaGithub } from 'react-icons/fa'
+import ItemContact from "../ItemContact"
+
+export default function Contactt() {
+  return (
+    <Container>
+      <Content>
+        <ItemContact 
+          IconFa={FaLinkedin} 
+          LinkContact="https://www.linkedin.com/in/felipe-matheus-34232b162/" 
+        />
+
+        <ItemContact 
+          IconFa={FaGithub} 
+          LinkContact="https://github.com/felipematheus1337" 
+        />
+       
+        <ItemContact 
+          IconFa={FaInstagram} 
+          LinkContact="https://www.instagram.com/felipematheusdev/" 
+        />
+        <ItemContact 
+          IconFa={FaEnvelope} 
+          LinkContact="lipehbr@gmail.com" 
+        />
+      </Content>
+    </Container>
+  )
+}
